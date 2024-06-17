f310a180131ab4e43f69871611cab6fd7c021a47
adamwinebarger <adam.k.winebarger@gmail.com>
2024-06-17 16:38:37 -0400

    Accidentally discarded a bunch of changes but I think this should have everything we worked on today. In any case, autoscroll should work in a pinch if we decide to do it the css way. Still need to look and see if we can do it with some javascript though because that would be preferable I think

--- main.js
+++ main.js
@@ -1,28 +1,52 @@
-function autoScrollIframe(iframe) {
-            const contentWindow = iframe.contentWindow;
-            let scrollPosition = 0;
-            let scrollDirection = 1; // 1 for down, -1 for up
+function autoscroll2() {
+    const scrollContainers = document.querySelectorAll('.iframe-container');
+    console.log("autoscroll2 fired");
 
-            function scrollStep() {
-                scrollPosition += scrollDirection; // Adjust the scroll speed as needed
-                contentWindow.scrollTo(0, scrollPosition);
+    scrollContainers.forEach((scrollContainer) => {
+        const iframe = scrollContainer.querySelector('.autoscroll');
 
-                if (scrollPosition >= contentWindow.document.body.scrollHeight - contentWindow.innerHeight) {
-                    scrollDirection = -1; // Change direction to up
-                } else if (scrollPosition <= 0) {
-                    scrollDirection = 1; // Change direction to down
-                }
-
-                window.requestAnimationFrame(scrollStep);
-            }
-
-            // Start the scrolling
-            window.requestAnimationFrame(scrollStep);
+        // If we don't find an autoscroll object within an iframe container, then there's no reason to continue with any of this
+        if (!iframe) {
+            console.log("no autoscroll object found. returning");
+            return;
         }
 
-        // Wait for all iframes to load before starting the scroll
-        document.querySelectorAll('iframe.auto-scroll').forEach(iframe => {
-            iframe.onload = function() {
-                autoScrollIframe(iframe);
-            };
-        });
+        let scrollPosition = 0;
+        let scrollDirection = 0.7; // This is actually scroll speed, essentially. Might come back and change the name
+        const minVisibleHeight = 100; // Minimum height of the content that should always be visible
+        const pauseDuration = 2000; // Pause duration in milliseconds
+        const initialDelay = 4000; // Initial delay before auto-scrolling starts
+
+        console.log("Made it to scrollstep");
+
+        function scrollStep() {
+            console.log("Inside scroll step");
+            scrollPosition += scrollDirection;
+            iframe.style.transform = `translateY(-${scrollPosition}px)`; // Well this is an interesting way to do this
+
+            // Case that we've hit the bottom
+            if (scrollPosition >= iframe.scrollHeight - scrollContainer.clientHeight - minVisibleHeight) {
+                scrollDirection = -0.7;
+                console.log('at bottom')
+                setTimeout(() => window.requestAnimationFrame(scrollStep), pauseDuration); // Pause at the bottom
+            } else if (scrollPosition <= 0) { // Case that we're at the top
+                scrollDirection = 0.7;
+                console.log('at top')
+                setTimeout(() => window.requestAnimationFrame(scrollStep), pauseDuration); // Pause at the top
+            } else {
+                console.log('scrolling')
+                window.requestAnimationFrame(scrollStep);
+            }
+        }
+
+        // Start the scrolling after the initial delay
+        setTimeout(() => window.requestAnimationFrame(scrollStep), initialDelay);
+        //scrollStep()
+    });
+}
+
+// Driver Code - kicks off our autoscroll function
+window.onload = function() {
+    //setTimeout(() => autoscroll2(), 2000)
+    autoscroll2()
+};
